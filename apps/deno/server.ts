226262b4d06d196cf6ba83c1770c5d572a9c2eff
Dj Walker-Morgan <codepope@gmail.com>
2020-12-09 12:05:53 +0000

    Updated example

--- apps/deno/server.ts
+++ apps/deno/server.ts
@@ -1,14 +1,14 @@
-import { serve } from "https://deno.land/std@v0.40.0/http/server.ts";
+import { serve } from "https://deno.land/std/http/server.ts";
 
-const { PORT = "8080" } = Deno.env();
+const PORT = parseInt(Deno.env.get("PORT") || "8080") || 8080;
 
 console.log("listening on", PORT);
 
 async function helloServer(){
-  for await (const req of serve(`:${PORT}`)) {
-    const enc = new TextEncoder();
+  for await (const req of serve({hostname:"0.0.0.0",port:PORT})) {
+    //const enc = new TextEncoder();
     const body = `Hello from Deno!`;
-    req.respond({ body: enc.encode(body+"\n"+JSON.stringify(req.headers)) });
+    req.respond({ status:200, body: body });
     console.log(`${req.method} ${req.url}`)
   }
 };
